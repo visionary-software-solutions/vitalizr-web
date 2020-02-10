d5a1aa53aecdaff14c1c6c7295025cd3a2ff0ca7
V <nick@visionary.software>
2020-02-10 01:24:40 -0800

    fix parsing omg how did I not unit test that, inexcusable

--- src/main/java/software/visionary/vitalizr/web/VitalizrService.java
+++ src/main/java/software/visionary/vitalizr/web/VitalizrService.java
@@ -4,10 +4,13 @@ import javax.inject.Singleton;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Singleton
 final class VitalizrService implements VitalService {
+
+    private Logger LOG = Logger.getLogger(VitalizrService.class.getSimpleName());
 
     @Override
     public List<Vital> getVitalById(final String type, final String id) {
@@ -33,8 +36,10 @@ final class VitalizrService implements VitalService {
     private List<Vital> toVitals(final String type, final String id, final String response) {
         final String[] parts = response.split("\u0023");
         return Arrays.stream(parts).map(record -> {
+            LOG.info("Record is " + record);
             final String[] fields = record.split("\u2049");
-            return new Vital(type, Instant.ofEpochMilli(Long.parseLong(fields[0])), Double.parseDouble(parts[1]), parts[2], id);
+            LOG.info("The fields are " + Arrays.toString(fields));
+            return new Vital(type, Instant.ofEpochMilli(Long.parseLong(fields[0])), Double.parseDouble(fields[1]), fields[2], id);
         }).collect(Collectors.toUnmodifiableList());
     }
 }
